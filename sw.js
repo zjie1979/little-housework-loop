cb2362b8ad55c590a826b5151687f69e1a5ddab2
zjie1979 <13360694@qq.com>
2026-07-20 00:51:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "little-housework-loop-github-v4";
+const CACHE_NAME = "little-housework-loop-github-v5";
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const APP_SHELL = [
   BASE_PATH,
   `${BASE_PATH}index.html`,
   `${BASE_PATH}styles.css`,
-  `${BASE_PATH}app.js`,
+  `${BASE_PATH}app.js?v=5`,
   `${BASE_PATH}manifest.webmanifest`,
   `${BASE_PATH}icon.svg`,
 ];
